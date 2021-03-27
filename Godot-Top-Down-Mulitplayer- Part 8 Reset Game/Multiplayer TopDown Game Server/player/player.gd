5a74cdb9bfe9ac04011477cd55f44033717e0c5b
Rayuse <73310246+Rayuse@users.noreply.github.com>
2021-03-27 13:48:41 -0500

    fixed part 8 and added missing files

---
+++ Godot-Top-Down-Mulitplayer- Part 8 Reset Game/Multiplayer TopDown Game Server/player/player.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+remote func update_player(transform):
+	rpc_unreliable("update_remote_player", transform)
+	
+remote func player_bullet():
+	rpc("spawn_bullet")
+
+remote func die():
+	rpc("player_died")
